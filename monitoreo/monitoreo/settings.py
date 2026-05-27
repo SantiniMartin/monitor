f96c8cdd9b6f712594c97490f02f05bc3550a259
Elias <eliasmorales.21@hotmail.com>
2026-05-27 17:45:14 -0300

    Desactivar modo debug en configuración de Django para producción

--- monitoreo/monitoreo/settings.py
+++ monitoreo/monitoreo/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://localhost','https://monitoreo-2026.onrender.com']
