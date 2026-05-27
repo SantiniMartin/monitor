4c385929ac69f55aa531ba3a1f8915b0c5b81e18
Elias <eliasmorales.21@hotmail.com>
2026-05-27 17:18:05 -0300

    Correccion para servidor

--- monitoreo/monitoreo/settings.py
+++ monitoreo/monitoreo/settings.py
@@ -26,12 +26,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://localhost','https://monitoreo-2026.onrender.com']
 
 
 # Application definition
