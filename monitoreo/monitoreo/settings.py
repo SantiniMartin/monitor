b75bfbe470b52e23a1d7303df4e2505bd516e517
Elias <eliasmorales.21@hotmail.com>
2026-06-01 03:58:14 -0300

    Agregar STATIC_ROOT en la configuración de Django para la gestión de archivos estáticos

--- monitoreo/monitoreo/settings.py
+++ monitoreo/monitoreo/settings.py
@@ -24,6 +24,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Quick-start development settings - unsuitable for production
