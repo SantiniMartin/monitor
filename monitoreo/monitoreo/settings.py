cc29560f731caf8e02b5bedf5e539d1af5a33263
Santini Martin <Martin_Santini219@outlook.es>
2026-09-10 08:16:46 -0300

    mod static

--- monitoreo/monitoreo/settings.py
+++ monitoreo/monitoreo/settings.py
@@ -22,8 +22,9 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    BASE_DIR / "static",
 ]
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
